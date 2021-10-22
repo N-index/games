529fa78e08e2487611b7a18486f21b2522b43c4e
N-index <bebtterthanme@gmail.com>
2021-10-22 17:06:32 +0800

    feat: placed a github link on the  header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,14 +9,45 @@
       />
       <span>小游戏</span>
     </n-text>
+    <n-a
+      href="https://github.com/N-index/games"
+      target="_blank"
+      id="github"
+      style="text-decoration: none"
+    >
+      <n-button :round="round" :circle="circle" color="#000">
+        <template #icon>
+          <n-icon><LogoGithub /></n-icon>
+        </template>
+        <span id="github-text">Github</span>
+      </n-button>
+    </n-a>
   </header>
 </template>
 
 <script>
-import { NText } from "naive-ui";
+import { NText, NButton, NIcon, NA } from "naive-ui";
+import { LogoGithub } from "@vicons/ionicons5";
 export default {
   components: {
     NText,
+    NButton,
+    NIcon,
+    NA,
+    LogoGithub,
+  },
+  data() {
+    return {
+      round: true,
+      circle: false,
+    };
+  },
+  created() {
+    const res = window.matchMedia("(max-width: 700px)");
+    [this.round, this.circle] = [!res.matches, res.matches];
+    res.onchange = ({ matches }) => {
+      [this.round, this.circle] = [!matches, matches];
+    };
   },
 };
 </script>
@@ -41,6 +72,18 @@ header {
 .ui-logo:hover img {
   animation-play-state: running;
 }
+#github {
+  position: fixed;
+  right: 1.2vw;
+}
+@media screen and (max-width: 700px) {
+  #github-text {
+    display: none;
+  }
+  #github-text .n-button__icon {
+    margin: 0 !important;
+  }
+}
 @keyframes in {
   from {
     transform: rotate(0deg);
@@ -51,7 +94,6 @@ header {
 }
 @media screen and (max-width: 600px) {
   header {
-    /*padding: 1.5rem .75rem;*/
     flex-direction: row;
     justify-content: center;
   }
